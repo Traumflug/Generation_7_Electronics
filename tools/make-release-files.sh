9f3445cca838275b673d5e4edc059798adc11a6a
Markus Hitter <mah@jump-ing.de>
2011-03-22 19:57:20 +0100

    Start a script for making release documents.
    The plan is to offer gerbers, schematics and layouts for the
    latest release in the repository, so everybody can look there,
    even with not having gEDA handy.
    
    For now, schematics are produced.

---
+++ tools/make-release-files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while [ ! -d .git ]; do cd ..; done
+
+BASE_DIR="${PWD}"
+DOC_DIR="${BASE_DIR}/release documents"
+cd "${BASE_DIR}"
+mkdir -p "${DOC_DIR}"
+rm -rf "${DOC_DIR}"/*
+
+for F in *.sch; do
+  PS_DOC="${DOC_DIR}/${F%.sch} Schematic.ps"
+  gschem -p -o "${PS_DOC}" -s "${BASE_DIR}/tools/print.scm" "$F"
+  cd "${DOC_DIR}"
+  ps2pdf "${PS_DOC}"
+  rm -f "${PS_DOC}"
+  cd "${BASE_DIR}"
+done
+

e1b5a7ad02c7532c51f2814adf7ebde5c71ac2b1
Markus Hitter <mah@jump-ing.de>
2011-03-22 20:17:59 +0100

    Release Maker: add a tag file showing the release.

--- tools/make-release-files.sh
+++ tools/make-release-files.sh
@@ -8,6 +8,10 @@ cd "${BASE_DIR}"
 mkdir -p "${DOC_DIR}"
 rm -rf "${DOC_DIR}"/*
 
+RELEASE=$(git tag | tail -1)
+touch "${DOC_DIR}/this is ${RELEASE}"
+unset RELEASE
+
 for F in *.sch; do
   PS_DOC="${DOC_DIR}/${F%.sch} Schematic.ps"
   gschem -p -o "${PS_DOC}" -s "${BASE_DIR}/tools/print.scm" "$F"
